513f0f55e3dd8d6e96864d7320e7b25d52c40315
김진현 Kim Jinhyun <79571893+kimjhyun0627@users.noreply.github.com>
2025-07-17 15:49:24 +0900

    feat: admin 페이지 페이지네이션 구현
    - html 코드 일부 수정
    - product 정렬 키 id로 변경
    - 테스트를 위해 스키마 일부 수정

--- src/main/java/gift/constant/PaginationConst.java
+++ src/main/java/gift/constant/PaginationConst.java
@@ -3,9 +3,9 @@ package gift.constant;
 public final class PaginationConst {
 
     public static final int WISH_PAGE_SIZE = 10;
-    public static final int PRODUCT_PAGE_SIZE = 20;
+    public static final int PRODUCT_PAGE_SIZE = 15;
     public static final String WISH_PAGE_KEY = "id";
-    public static final String PRODUCT_PAGE_KEY = "name";
+    public static final String PRODUCT_PAGE_KEY = "id";
 
     private PaginationConst() {
     }
